267732b5dd43dd88350e6e08b20984c838c62eab
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-21 23:22:22 +0100

    Add first Kotlin class Person

---
+++ src/ua/com/sevenpowerx/kotlin/book/splaa/Person.kt
@@ -0,0 +1,12 @@
+package ua.com.sevenpowerx.kotlin.book.splaa
+
+data class Person(val name: String,
+                  val age: Int? = null)
+
+fun main(args: Array<String>) {
+    val persons = listOf(Person("Alice"),
+            Person("Bob", age = 29))
+    val oldest = persons.maxBy { it.age ?: 0 }
+    println("The oldest is: $oldest")
+
+}
