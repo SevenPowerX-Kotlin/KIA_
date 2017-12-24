6ee62723294c2623be2f87920837032556340aee
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 16:29:49 +0100

    update Person.kt file (class) Changed README.md file

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 стр - 26. 1.2.2.Статическая типизация
 TornadoFx 
 JavaFx
+
+http://github.com/jetbrains/kotlin
+
+Sql https://github.com/jetbrains/exposed
+
+<<Tutorials>> - https://kotlinlang.org/docs/tutorials
+
+Online палигон http://try.kotlin
+
--- src/ua/com/sevenpowerx/kotlin/book/splaa/Person.kt
+++ src/ua/com/sevenpowerx/kotlin/book/splaa/Person.kt
@@ -2,6 +2,9 @@ package ua.com.sevenpowerx.kotlin.book.splaa
 
 data class Person(val name: String,
                   val age: Int? = null)
+fun findPerso(function: () -> Boolean): Any {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
 
 fun main(args: Array<String>) {
     val persons = listOf(Person("Alice"),
@@ -9,13 +12,41 @@ fun main(args: Array<String>) {
     val oldest = persons.maxBy { it.age ?: 0 }
     println("The oldest is: $oldest")
 
+    val s: String? = null
+    val s2: String = ""
+    val s3: String = "Hello, Kotlin!!!"
+    val value = s3
+
+
+    if(value is String)
+        println(value.toUpperCase())
+    if(value is String)
+        println(value.toLowerCase())
+
+
 }
 
-fun findPerso(function: () -> Boolean): Any {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-}
 
 //fun findAlice() = findPerso{it.name == "Alice"}
 
 
 //fun findBob() = findPerson{it.name == "Bob"}
+/*
+fun renderPersonList(persons: Collection<Person>)=
+        create HTML().table{
+            for (person in persons) {
+                td{ +person.name }
+                td{ +person.age }
+
+            }
+        }*/
+
+//fun createHTML(): Any {}
+/*
+
+object CountryTable : IdTable(){
+    val name = varchar("name", 250).uniqueIndex
+
+}*/
+
+
