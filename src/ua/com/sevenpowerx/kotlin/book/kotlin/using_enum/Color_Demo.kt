8dd109ee2d22303bf9c0d35d0fab48b44011f207
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 21:21:02 +0100

    2.3.3 Использование оператора "when" с произвольными объектами

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/using_enum/Color_Demo.kt
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/using_enum/Color_Demo.kt
@@ -2,6 +2,14 @@ package ua.com.sevenpowerx.kotlin.book.kotlin.using_enum
 
 import ua.com.sevenpowerx.kotlin.book.kotlin.using_enum.Color.*
 
+
+fun main(args: Array<String>) {
+    println(getMnemonic(BLUE))
+    println(getWarmth(BLUE))
+    println(mix(BLUE,YELLOW))
+
+}
+
 fun getMnemonic(color: Color) =
         when (color) {
 
@@ -14,15 +22,18 @@ fun getMnemonic(color: Color) =
             INDIGO -> "Сидит?"
             VIOLET -> "Фазан?"
         }
-
 fun getWarmth(color: Color) = when(color) {
     RED, ORANGE, YELLOW -> "тёплый"
     GREEN -> "нейтральный"
     BLUE,INDIGO,VIOLET -> "холодный"
 }
 
-fun main(args: Array<String>) {
-    println(getMnemonic(BLUE))
-    println(getWarmth(BLUE))
+fun mix(c1: Color, c2: Color)=
+    when (setOf(c1, c2)) {
+        setOf(RED,YELLOW)-> ORANGE
+        setOf(BLUE,YELLOW)->GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Грязный цвет")
+    }
 
-}
+
