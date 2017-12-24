dbd067e3b5393896ce2319c90e8f638fcb37e450
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 22:20:21 +0100

    2.3.6.часть 2 замена if на when

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/using_enum/auto_type/Expr_demo.kt
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/using_enum/auto_type/Expr_demo.kt
@@ -5,10 +5,9 @@ fun main(args: Array<String>) {
 }
 
 fun eval(e: Expr): Int =
-    if (e is Num) {
-       e.value
-    }else if (e is Sum) {
-        eval(e.right)+ eval(e.left)
-    }else {
+   when(e){
+       is Num -> e.value
+       is Sum -> eval(e.right) + eval(e.left)
+       else ->
         throw IllegalArgumentException("Unknown expression")
     }
