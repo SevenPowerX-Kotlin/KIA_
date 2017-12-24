aded875edc0d22d0a157ac6fb782529abcb0b3e3
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 23:33:32 +0100

    2.4.1 Цикл while

---
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/used_while/While.kt
@@ -0,0 +1,12 @@
+package ua.com.sevenpowerx.kotlin.book.kotlin.used_while
+
+fun main(args: Array<String>) {
+    val condition = false
+    while (condition){
+        /*...........*/
+    }
+    do {
+        /*...*/
+    }while (condition)
+
+}
