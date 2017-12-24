9a4c5f8da1222bdc1f0295f527a0e51afdd08e28
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 23:58:43 +0100

    2.4.2. Итерация по последовательности чисел: диапазоны и прогрессии

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/used_while/While.kt
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/used_while/While.kt
@@ -1,12 +1,29 @@
 package ua.com.sevenpowerx.kotlin.book.kotlin.used_while
 
 fun main(args: Array<String>) {
-    val condition = false
+  /*  val condition = false
     while (condition){
-        /*...........*/
+        *//*...........*//*
     }
     do {
-        /*...*/
-    }while (condition)
+        *//*...*//*
+    }while (condition)*/
 
+
+   /* for (i in 1..100) {
+       if(i%10!=0)
+        print(fizzBuzz(i))
+        else
+        println(fizzBuzz(i))
+    }*/
+    for (i in 100 downTo 1 step 2) {
+        print(fizzBuzz(i))
+    }
+}
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
 }
