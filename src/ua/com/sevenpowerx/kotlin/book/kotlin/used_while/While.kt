00c6737ce1a089653893602b841a88b5e2ac3df0
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-25 00:16:51 +0100

    2.4.3. Итерация по элементам словарей

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/used_while/While.kt
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/used_while/While.kt
@@ -1,16 +1,18 @@
 package ua.com.sevenpowerx.kotlin.book.kotlin.used_while
 
+import java.util.*
+
 fun main(args: Array<String>) {
   /*  val condition = false
     while (condition){
-        *//*...........*//*
+        ...........
     }
     do {
-        *//*...*//*
-    }while (condition)*/
+        ...
+    }while (condition)
 
 
-   /* for (i in 1..100) {
+    for (i in 1..100) {
        if(i%10!=0)
         print(fizzBuzz(i))
         else
@@ -18,6 +20,23 @@ fun main(args: Array<String>) {
     }*/
     for (i in 100 downTo 1 step 2) {
         print(fizzBuzz(i))
+    }
+    /*
+    for (x in 0 until size)
+        for(x in 0..size-1)*/
+
+    val binaryReps = TreeMap<Char, String>()
+    for (c in 'A'..'F') {
+        val binary = Integer.toBinaryString(c.toInt())
+        binaryReps[c] = binary
+    }
+    for ((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
+
+    val list = arrayListOf("10", "11", "1001")
+    for ((index, element)in list.withIndex()){
+        println("$index: $element")
     }
 }
 
