d821bda2f293d9328bfccd5bf71ba55900d5c89d
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 17:48:41 +0100

    2.1.3.Переменные

--- src/ua/com/sevenpowerx/kotlin/book/splaa/Hello.kt
+++ src/ua/com/sevenpowerx/kotlin/book/splaa/Hello.kt
@@ -14,5 +14,39 @@ fun max(a: Int, b: Int) = if(a>b) a else b
 fun main(args: Array<String>): Unit {
     println("Hello, Kotlin!")
     println(max(1, 2))
+
+    val question =
+            "The Ultimate Question of Life, the Universe, and Everything"
+    val answer = 42
+//    answer = "no answer"  //ошибка
+
+    val answerSplx: Int = 25
+
+    val yearToComputer = 7.5e6
+
+    val answerAndrey: Int //указывать явно
+    answerAndrey = 40
+
+
+    //val(от value) - неизменяемая ссылка
+    //var(от variable - изменяемая ссылка
+
+    val message: String
+    if (canPerformOperation()) {
+        message = "Success"
+        // ... выполнить операцию
+    }
+    else{
+        message = "Failed"
+    }
+    val languages = arrayListOf<String>("Java")
+    languages.add("Kotlin")
+
+
+
+}
+
+fun canPerformOperation(): Boolean {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 }
 
