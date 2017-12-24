4284d472241a52dffb6523cd4ede430ced70bc2c
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 19:48:18 +0100

    2.2.3.Размещение исходного кода на Kotlin: пакеты и каталоги

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/Rectangle.kt
+++
@@ -1,8 +0,0 @@
-package ua.com.sevenpowerx.kotlin.book.kotlin
-
-class Rectangle(val height: Int, val width: Int) {
-    val isSquare: Boolean
-    get(){
-        return height == width
-    }
-}
---
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/geometry/example/Example.kt
@@ -0,0 +1,7 @@
+package ua.com.sevenpowerx.kotlin.book.kotlin.geometry.example
+
+import ua.com.sevenpowerx.kotlin.book.kotlin.geometry.shapes.createRandomRectangle
+
+fun main(args: Array<String>) {
+    println(createRandomRectangle().isSquare)
+}
