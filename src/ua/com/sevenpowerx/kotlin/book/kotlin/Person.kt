05e192467638785e7b8573253f68e41a6842957e
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 19:02:53 +0100

    2.2.1 Свойства

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/Person.kt
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/Person.kt
@@ -1,53 +1,7 @@
 package ua.com.sevenpowerx.kotlin.book.kotlin
 
-data class Person(val name: String,
-                  val age: Int? = null)
-fun findPerso(function: () -> Boolean): Any {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-}
-
-fun main(args: Array<String>) {
-    val persons = listOf(Person("Alice"),
-            Person("Bob", age = 29))
-    val oldest = persons.maxBy { it.age ?: 0 }
-    println("The oldest is: $oldest")
-
-    val s: String? = null
-    val s2: String = ""
-    val s3: String = "Hello, Kotlin!!!"
-    val value = s3
-
-
-    if(value is String)
-        println(value.toUpperCase())
-    if(value is String)
-        println(value.toLowerCase())
-
-
-}
-
-
-
-//fun findAlice() = findPerson{it.name == "Alice"}
-
-
-//fun findBob() = findPerson{it.name == "Bob"}
-/*
-fun renderPersonList(persons: Collection<Person>)=
-        create HTML().table{
-            for (person in persons) {
-                td{ +person.name }
-                td{ +person.age }
-
-            }
-        }*/
-
-//fun createHTML(): Any {}
-/*
-
-object CountryTable : IdTable(){
-    val name = varchar("name", 250).uniqueIndex
-
-}*/
-
-
+data class Person(
+        val name: String,       //Неизменяемое свойство: для него будут созданы поле и простой метод чтения
+        val age: Int? = null,   //Неизменяемое свойство: для него будут созданы поле и простой метод чтения
+        var isMarried: Boolean  //Изменяемое свойство:поле, методы чтения и записи
+)
