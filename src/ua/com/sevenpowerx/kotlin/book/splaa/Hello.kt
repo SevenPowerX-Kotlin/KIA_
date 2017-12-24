a90c4395a5b6962dc3c0e6298655423753c775a8
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-24 17:15:01 +0100

    2.1.2.Функци

--- src/ua/com/sevenpowerx/kotlin/book/splaa/Hello.kt
+++ src/ua/com/sevenpowerx/kotlin/book/splaa/Hello.kt
@@ -1,5 +1,18 @@
 package ua.com.sevenpowerx.kotlin.book.splaa
 
+
+//  Функция имеет тело-блок(block body)
+fun max(a: String, b: String): String {
+    return if(a>b) "a" else "b"
+}
+    //Функция имеет тело-выражения(expression body)
+//fun max(a: Int, b: Int): Int = if(a>b) a else b
+fun max(a: Int, b: Int) = if(a>b) a else b
+
+
+
 fun main(args: Array<String>): Unit {
     println("Hello, Kotlin!")
+    println(max(1, 2))
 }
+
