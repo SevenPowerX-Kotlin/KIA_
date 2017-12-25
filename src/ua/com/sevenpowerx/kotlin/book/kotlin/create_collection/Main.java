94fe4f3b6e202e48d93d242ceec575563fd60943
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-25 16:45:29 +0100

    3.2.3.Избавление от статических вспомагательных классов:свойства и функции верхнего уровня

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/create_collection/Main.java
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/create_collection/Main.java
@@ -1,7 +1,5 @@
 package ua.com.sevenpowerx.kotlin.book.kotlin.create_collection;
 
-import strings.StringFunctions;
-
 public class Main {
-	StringFunctions.joinToString(list, "","","")
+//	StringFunctions.joinToString(list, "","","")
 }
