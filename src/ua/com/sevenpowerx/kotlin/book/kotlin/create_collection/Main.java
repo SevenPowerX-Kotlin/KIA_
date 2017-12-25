40fb20f3955f94840a938bef7a8399e0f71932cc
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-25 17:21:37 +0100

    3.3.1.Дериктив импорта и функциии-расширения

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/create_collection/Main.java
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/create_collection/Main.java
@@ -1,5 +1,7 @@
 package ua.com.sevenpowerx.kotlin.book.kotlin.create_collection;
 
+//import strings.StringFunctions.*;
+
 public class Main {
 //	StringFunctions.joinToString(list, "","","")
 }
---
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/used_import/usedImp.kt
@@ -0,0 +1,11 @@
+package ua.com.sevenpowerx.kotlin.book.kotlin.used_import
+
+//import strings.lastChar
+//import strings.*
+import strings.lastChar as last //меняем имя импортируемого класса или функции
+
+
+//val  c = "Kotlin".lastChar()
+val  c = "Kotlin".last()
+
+
