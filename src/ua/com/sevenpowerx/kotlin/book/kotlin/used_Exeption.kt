404b085ba3045254c1b2b6843a504256ad56fb00
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-25 10:12:52 +0100

    2.5.1 try, catch и finally

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/used_Exeption.kt
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/used_Exeption.kt
@@ -1,11 +1,26 @@
 package ua.com.sevenpowerx.kotlin.book.kotlin
 
+import java.io.BufferedReader
+import java.io.StringReader
+
 fun main(args: Array<String>) {
-    val percentage = 1000
+    val percentage = 100
     if (percentage !in 0..100) {
         throw IllegalArgumentException(
                 "A percentage value must be between 0 and 100: $percentage"
         )
     }
+    val reader = BufferedReader(StringReader("239"))
+    println(readNumber(reader))
 }
 
+fun readNumber(reader: BufferedReader): Int? {
+    return try {
+        val line = reader.readLine()
+        Integer.parseInt(line)
+    } catch (e: NumberFormatException) {
+        null
+    } finally {
+        reader.close()
+    }
+}
