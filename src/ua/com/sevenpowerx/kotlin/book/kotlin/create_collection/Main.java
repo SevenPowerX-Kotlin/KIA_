49156966ba69c422c442eb3e82b27059730209a0
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-25 17:29:29 +0100

    3.3.2.Вызов функций-расширений из Java

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/create_collection/Main.java
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/create_collection/Main.java
@@ -2,6 +2,14 @@ package ua.com.sevenpowerx.kotlin.book.kotlin.create_collection;
 
 //import strings.StringFunctions.*;
 
+import strings.StringFunctions;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 public class Main {
-//	StringFunctions.joinToString(list, "","","")
+	private Collection<String> list = Arrays.asList("1", "2", "3");
+	
+	String str = StringFunctions.joinToString(list, "","","");
+	char c = StringFunctions.lastChar("Java");
 }
