2431569ccd6e6177ba5654705bb6a704acdc14a4
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-26 09:26:44 +0100

    3.5.1.Разбиение строк 2

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/work_regex/DemoRegex.kt
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/work_regex/DemoRegex.kt
@@ -1,5 +1,10 @@
 package ua.com.sevenpowerx.kotlin.book.kotlin.work_regex
 
 fun main(args: Array<String>) {
-	println("12.345-6.A".split("\\.|-".toRegex()))
+	val str = "12.345-6.A"
+	println(str)
+	println(str.split("."))
+	println(str.split(".", "-"))
+	println(str.split("\\.|-".toRegex()))
+
 }
