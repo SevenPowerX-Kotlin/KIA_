90e399f28a036e8f4924d9bee99f3fe9acfa3c46
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-26 09:53:51 +0100

    3.5.2.Регулярные выражения и строки в тройных кавычках

--- src/ua/com/sevenpowerx/kotlin/book/kotlin/work_regex/DemoRegex.kt
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/work_regex/DemoRegex.kt
@@ -7,4 +7,32 @@ fun main(args: Array<String>) {
 	println(str.split(".", "-"))
 	println(str.split("\\.|-".toRegex()))
 
+	println("Parse Path")
+	val path = "/Users/yole/kotlin-book/chapter.adoc"
+	println(parsePath(path))
+	println("reg")
+	println(parsePathToReg(path))
+
+}
+
+fun parsePath(path: String): String {
+	val directory = path.substringBeforeLast("/")
+	val fullName = path.substringAfterLast("/")
+
+	val fileName = fullName.substringBeforeLast(".")
+	val extension = fullName.substringAfterLast(".")
+
+	return "Dir: $directory, name: $fileName, ext: $extension"
+}
+
+fun parsePathToReg(path: String): String {
+	var str: String = ""
+	val regex = """(.+)/(.+)\.(.+)""".toRegex()
+	val matchResult = regex.matchEntire(path)
+	if (matchResult != null) {
+		val(directory, fileName, extension) = matchResult.destructured
+		str = "Dir: $directory, name: $fileName, ext: $extension"
+	}else str = "Null"
+
+	return str
 }
