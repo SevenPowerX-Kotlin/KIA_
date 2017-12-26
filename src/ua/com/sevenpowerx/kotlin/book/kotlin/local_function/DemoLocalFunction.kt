870b3e151abae5295ee1a5867b909b0696f5d56b
Acer Notebook Андрей Лавринюк <splaandrey@gmail.com>
2017-12-26 11:09:31 +0100

    3.6.Чистим код: локальные функции и расширения

---
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/local_function/DemoLocalFunction.kt
@@ -0,0 +1,7 @@
+package ua.com.sevenpowerx.kotlin.book.kotlin.local_function
+
+fun main(args: Array<String>) {
+
+	saveUser(User(1, "", ""))
+
+}
---
+++ src/ua/com/sevenpowerx/kotlin/book/kotlin/multi_literal/DemoMultiLiteral.kt
@@ -0,0 +1,9 @@
+package ua.com.sevenpowerx.kotlin.book.kotlin.multi_literal
+
+fun main(args: Array<String>) {
+	val kotlinlogo =  """
+							| //
+							|//
+							|/ \"""
+	println(kotlinlogo)
+}
